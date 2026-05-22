a512ac1e7ff76f15dff9bbfd818f965e68932178
LoraineCastr <loraine.castro.1214@miremington.edu.co>
2026-05-21 22:34:34 -0500

    Actividad 5 Modificar elementos

--- Tienda_de_frutas.py
+++ Tienda_de_frutas.py
@@ -4,12 +4,12 @@ list=["mandarina","lulo","fresa","maracuya","limon"]
 print(list[0],list[4])
 
 #Actividad 2
-print(list[2])
-print(list[10]) 
+print(list[2])  
+#print(list[10])  #posicion inexistente
 #El sistema genera un error, ya que no exite un elemento en la posicion que se esta ingresando.
 
 # Actividad 3
-list.append("melon")
+list.append("melon")   
 print(list)
 
 list.insert(2,"cereza")
@@ -29,3 +29,14 @@ print(list)
 # remove - elimina el elemento que se le indique, en este caso "fresa"
 # pop - elimina el ultimo elemento de la lista, en este caso "melon"    
 # pop(4) - elimina el elemento que se encuentra en la posicion 4, en este caso "limon"
+
+
+# #Actividad 5
+
+print(list) #Lista antes del cambio
+
+list[1] = 'sandia'
+print(list) #Lista despues del cambio
+
+
+
