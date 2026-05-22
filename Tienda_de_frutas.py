37d550b7b6da8920b2e9c7cd62cb98026d712958
LoraineCastr <loraine.castro.1214@miremington.edu.co>
2026-05-21 20:12:04 -0500

    Actividad 2 Acceder a posiciones

--- Tienda_de_frutas.py
+++ Tienda_de_frutas.py
@@ -1,4 +1,12 @@
-#frutas 
+#Actividad 1  -frutas 
 list=["mandarina","lulo","fresa","maracuya","limon"]
 # print(list)
 print(list[0],list[4])
+
+#Actividad 2
+print(list[2])
+print(list[10]) 
+#El sistema genera un error, ya que no exite un elemento en la posicion que se esta ingresando.
+
+
+
